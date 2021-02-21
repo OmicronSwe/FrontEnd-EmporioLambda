67c6e06195a9dbc8708b225223ea463ce651a3c2
MatthewBalzan <matthewb487@gmail.com>
2021-02-21 20:27:47 +0100

    preventing deploy error

--- pages/profile.tsx
+++ pages/profile.tsx
@@ -1,11 +1,8 @@
-
 import { getSession } from "next-auth/client"
 import Layout from "../components/layout"
 import { GetServerSideProps } from "next"
 
-
 const ProfilePage = ({ session }) => {
-
   if (!session)
     return (
       <Layout title="Profile | Next.js + TypeScript Example">
@@ -24,7 +21,6 @@ const ProfilePage = ({ session }) => {
     </Layout>
   )
 }
-
 
 export const getServerSideProps: GetServerSideProps = async ({ req }) => {
   const session = await getSession({ req })
